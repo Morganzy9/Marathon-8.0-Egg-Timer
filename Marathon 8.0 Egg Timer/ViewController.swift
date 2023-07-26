3621e78b77e003f797ab489ab0772ca888e018b7
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-07-26 14:27:53 +0500

    Initial Commit

---
+++ Marathon 8.0 Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon 8.0 Egg Timer
+//
+//  Created by Ислам Пулатов on 7/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
