1095fafdf869b7268f04d1b89b19d671c9dfc393
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-07-26 15:59:13 +0500

    -Projecct Done

--- Marathon 8.0 Egg Timer/ViewController.swift
+++ Marathon 8.0 Egg Timer/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var progressBar: UIProgressView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var timer = Timer()
+    
+    var player: AVAudioPlayer!
+    
+    var totalTime = 0
+    
+    var secondsPassed = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    @IBAction func hardnessPressed(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        guard let hardness = sender.currentTitle else { return }
+        
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        
+        secondsPassed = 0
+        
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+        
+    }
+    
+    
+    @objc func updateTimer() {
+        
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+            print(Float(secondsPassed) / Float(totalTime))
+            
+        } else {
+        
+            timer.invalidate()
+            
+            titleLabel.text = "DONE!"
+            
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            
+            player = try! AVAudioPlayer(contentsOf: url!)
+            
+            player.play()
+        }
+        
+    }
 }
 
